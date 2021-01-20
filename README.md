75e6c592e0af877e7676582aeb753095bdb13c52
Ashley <ash.r.diep@gmail.com>
2021-01-20 17:24:13 -0500

    Add basic mathematical functionality

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## The Odin Project | Foundations
 **Project: Calculator**
+
+Things to Do
+- Fix precision for possible large results
--- scripts.js
+++ scripts.js
@@ -18,8 +18,7 @@ function divide(num1, num2)
     let quotient = 0;
     if (num2 == 0)
     {
-        alert("So you have chosen death.");
-        quotient = -1;
+        updateDisplay("DEATH");
     }
     else
     {
@@ -50,3 +49,88 @@ function operate(operator, num1, num2)
 
     return result;
 }
+
+function updateDisplay(val)
+{
+    if (displayVal.length >= 9) 
+    {
+        alert("Cannot display more values");
+        return;
+    }
+
+    if (currentVal == 0)
+    {
+        displayVal = val;
+        currentVal = val;
+    }
+    else
+    {
+        displayVal += val;
+        currentVal = Number(displayVal);
+    }
+
+    let display = document.getElementById("display");
+    display.innerHTML = displayVal;
+}
+
+function saveOperation()
+{
+    // When there is no new number and operation is changed.
+    if (currentVal == 0)
+    {
+        operations[0] = this.id;
+        return;
+    }
+
+    currentVal = 0;
+    inputs.push(Number(displayVal));
+    operations.push(this.id);
+
+    if (inputs.length == 2)
+    {
+        let result = operate(operations[0], inputs[0], inputs[1]);
+        inputs.unshift(result);
+        inputs.pop(); 
+        inputs.pop();
+        operations.shift();
+        updateDisplay(result);
+        currentVal = 0;
+    }
+
+}
+
+function clearDisplay()
+{
+    // When C is clicked, it clears the current screen to 0
+}
+
+function clearAll()
+{
+    // When AC is clicked, it clears all operations and inputs
+}
+
+let displayVal = "0";
+let currentVal = 0;
+let operations = [];
+let inputs = [];
+
+updateDisplay(displayVal);
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach(number => number.addEventListener("click", () => updateDisplay(number.id)));
+
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach(operator => operator.addEventListener("click", saveOperation));
+
+const equalsButton = document.getElementById("equals");
+equalsButton.addEventListener("click", function() {
+    inputs.push(Number(displayVal))
+    operate(operations[0], inputs[0], inputs[1]);
+    let result = operate(operations[0], inputs[0], inputs[1]);
+    inputs.unshift(result);
+    inputs.pop(); 
+    inputs.pop();
+    operations.shift();
+    currentVal = 0;
+    updateDisplay(result);
+    currentVal = 0;
+});
