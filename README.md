6ee7d4bc7c37abf59176dca67f358b8a66169f08
Ashley <ash.r.diep@gmail.com>
2021-01-20 23:22:03 -0500

    Clean up code and fix button functions

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
 ## The Odin Project | Foundations
 **Project: Calculator**
 
-As of right now: everything works, although in the ugliest way possible. 
-things that have problems:
-- % display
-- . display
-- negation
-
-Things to Do
-- Clean up code for everything
-- Fix precision for possible large results
-- find a better font? 
-- color coordinate the stuff
+A calculator with basic arithmetic operations created with HTML, CSS, and JavaScript.
--- scripts.js
+++ scripts.js
@@ -18,14 +18,14 @@ function divide(num1, num2)
     let quotient = 0;
     if (num2 == 0)
     {
-        updateDisplay("DEATH");
+        alert("Death.");
     }
     else
     {
         quotient = num1 / num2;
     }
 
-    return Number(quotient.toFixed(6));
+    return quotient;
 }
 
 function operate(operator, num1, num2)
@@ -50,86 +50,95 @@ function operate(operator, num1, num2)
     return result;
 }
 
-function updateDisplay(val)
+function initializeDisplay()
+{
+    display.innerHTML = displayVal;
+    newValNext = true;
+}
+
+function updateDisplay(newDisplay)
 {
     if (displayVal.length >= 9) 
     {
         alert("Cannot display more values");
         return;
     }
+    
+    if (newDisplay == "0" && displayVal == "0") return;
 
-    if (currentVal == 0)
+    if (newValNext)
     {
-        displayVal = val;
-        currentVal = val;
+        displayVal = newDisplay;
+        newValNext = false;
     }
     else
     {
-        displayVal += val;
-        currentVal = Number(displayVal);
+        displayVal += newDisplay;
     }
 
-    let display = document.getElementById("display");
     display.innerHTML = displayVal;
 }
 
 function saveOperation()
 {
-    // When there is no new number and operation is changed.
-    if (currentVal == 0)
+    if (newValNext)
     {
-        operations[0] = this.id;
+        operation = this.id;
         return;
     }
 
-    currentVal = 0;
-    inputs.push(Number(displayVal));
-    operations.push(this.id);
+   inputs.push(Number(displayVal));
+   newValNext = true;
 
-    if (inputs.length == 2)
-    {
-        let result = operate(operations[0], inputs[0], inputs[1]);
-        inputs.unshift(result);
-        inputs.pop(); 
-        inputs.pop();
-        operations.shift();
-        updateDisplay(result);
-        currentVal = 0;
-    }
+   if (inputs.length == 2)
+   {
+       parseExpression();
+   }
 
+    operation = this.id;
 }
 
-function clear()
+function parseExpression() 
 {
-    currentVal = 0;
-    displayVal = "0";
-    updateDisplay(displayVal);
-    console.log("APIN");
+    let result = operate(operation, inputs[0], inputs[1]);
+    inputs[0] = result;
+    inputs.pop();
+
+    newValNext = true;
+    updateDisplay(result.toString());
+    newValNext = true;
 }
 
 function clearAll()
 {
-    while (operations.length)
-    {
-        operations.pop();
-    }
-
     while (inputs.length)
     {
         inputs.pop();
     }
 
+    operation = "";
     displayVal = "0";
-    currentVal = 0;
-    updateDisplay(displayVal);
+    newValNext = true;
+    initializeDisplay();
+    decimalButton.disabled = false;
+}
+
+function clear()
+{
+    displayVal = "0";
+    newValNext = true;
+    initializeDisplay();
+    decimalButton.disabled = false;
 }
 
 let displayVal = "0";
-let currentVal = 0;
-let operations = [];
+let newValNext = true;
 let inputs = [];
+let operation = "";
+let display = document.getElementById("display");
 
-updateDisplay(displayVal);
+initializeDisplay();
+
 const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach(number => number.addEventListener("click", () => updateDisplay(number.id)));
 
@@ -139,16 +148,8 @@ operatorButtons.forEach(operator => operator.addEventListener("click", saveOpera
 const equalsButton = document.getElementById("equals");
 equalsButton.addEventListener("click", function() {
     if (inputs.length == 0) return;
-    inputs.push(Number(displayVal))
-    operate(operations[0], inputs[0], inputs[1]);
-    let result = operate(operations[0], inputs[0], inputs[1]);
-    inputs.unshift(result);
-    inputs.pop(); 
-    inputs.pop();
-    operations.shift();
-    currentVal = 0;
-    updateDisplay(result);
-    currentVal = 0;
+    updateDisplay(inputs[0].toString());
+    newValNext = true;
 });
 
 const clearAllButton = document.getElementById("clear-all");
@@ -161,22 +162,39 @@ const decimalButton = document.getElementById("decimal");
 decimalButton.disabled = false;
 decimalButton.addEventListener("click", function() {
     displayVal += ".";
+    newValNext = true;
     updateDisplay(displayVal);
     decimalButton.disabled = true;
 });
 
 const negationButton = document.getElementById("negation");
 negationButton.addEventListener("click", function() {
-    // check if already negative
-    let newVal = "-";
-    displayVal = newVal.concat(displayVal);
+    if (displayVal == "0") return;
+    if (displayVal[0] == "-")
+    {
+        displayVal = displayVal.slice(1);
+    }
+    else
+    {
+        displayVal = "-".concat(displayVal);
+    }
+
+    newValNext = true;
+
+    if (-(inputs[0]) == Number(displayVal))
+    {
+        inputs.pop();
+    }
+
     updateDisplay(displayVal);
+
 });
 
 const percentButton = document.getElementById("percent");
 percentButton.addEventListener("click", function() {
     let percentNum = Number(displayVal);
     percentNum /= 100;
-    displayVal = percentNum.toString();
+    displayVal = (percentNum).toString();
+    newValNext = true;
     updateDisplay(displayVal);
 })
